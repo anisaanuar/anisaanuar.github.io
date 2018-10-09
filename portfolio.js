364f54bf25662f5dbd14558167ed6edbda5212e4
Anisa <anisaanuar98@gmail.com>
2018-10-09 16:28:26 -0400

    Made a gallery!!
    :D

--- portfolio.js
+++ portfolio.js
@@ -21,3 +21,46 @@ function toggleMenu() {
     x.style.display = "block";
   }
 }
+
+// GALLERY JS
+
+// Open the Modal
+function openGallery() {
+  document.getElementById('photoGallery').style.display = "block";
+}
+
+// Close the Modal
+function closeGallery() {
+  document.getElementById('photoGallery').style.display = "none";
+}
+
+var slideIndex = 1;
+showGallery(slideIndex);
+
+// Next/previous controls
+function plusBlock(n) {
+  showGallery(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentBlock(n) {
+  showGallery(slideIndex = n);
+}
+
+function showGallery(n) {
+  var i;
+  var slides = document.getElementsByClassName("gallery-block");
+  var dots = document.getElementsByClassName("gallery-photo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
