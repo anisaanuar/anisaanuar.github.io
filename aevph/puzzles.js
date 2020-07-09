2524c1516c052ad6ac86a86613b9b7263802bde6
Anisa <anisaanuar98@gmail.com>
2020-07-09 18:39:06 -0400

    aevph

---
+++ aevph/puzzles.js
@@ -0,0 +1,29 @@
+function goBack() {
+    window.history.back();
+}
+
+document.getElementById("pswd").addEventListener("keypress", function(e) {
+    var key = e.which || e.keyCode || 0;
+    if (key === 13) {
+        e.preventDefault();
+        checkPswd();
+    }
+});
+
+document.getElementById("submitButton").addEventListener("click", checkPswd);
+
+function checkPswd() {
+    var password = document.getElementById("pswd").value.toLowerCase();
+    switch (password) {
+        case ('aevph'):
+            window.location = "welcome.html";
+            break;
+        case('check my socials'):
+        case('checkmysocials'):
+        	window.location = "jointhehunt.html";
+        	break;
+        default:
+            alert("Not a valid password.");
+            break;
+    }
+}
