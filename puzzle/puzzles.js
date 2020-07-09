6ca67cf924d3a79cbb4aecb4e0b18aabf0e56c5e
Anisa <anisaanuar98@gmail.com>
2020-07-09 17:05:43 -0400

    intro

--- puzzle/puzzles.js
+++ puzzle/puzzles.js
@@ -1,35 +1,47 @@
+function goBack() {
+    window.history.back();
+}
+
 document.getElementById("pswd").addEventListener("keypress", function(e) {
-	var key = e.which || e.keyCode || 0;
-	if (key === 13) {
-		e.preventDefault(); 
-		checkPswd();
-	}
+    var key = e.which || e.keyCode || 0;
+    if (key === 13) {
+        e.preventDefault();
+        checkPswd();
+    }
 });
 
 document.getElementById("submitButton").addEventListener("click", checkPswd);
 
 function checkPswd() {
-	var password = document.getElementById("pswd").value;
-	switch (password) {
-		case ('example'):
-		window.location = "example.html";
-		break;
-		case ('dash'):
-		window.location = "teamname.html";
-		break;
-		case ('crossword'):
-		window.location = "crossword.html";
-		break;
-		case ('wordsearch'):
-		window.location = "wordsearch.html";
-		break;
-		default:
-		alert("Not a valid password.");
-		break;
-	}
-}
-
-
-function goBack() {
-	window.history.back();
+    var password = document.getElementById("pswd").value.toLowerCase();
+    switch (password) {
+        case ('example'):
+            window.location = "example.html";
+            break;
+        case ('intro'):
+            window.location = "intro.html";
+            break;
+        case('check my socials'):
+        case('checkmysocials'):
+        	window.location = "jointhehunt.html";
+        	break;
+        case ('dash'):
+            window.location = "teamname.html";
+            break;
+        case ('crossword'):
+            window.location = "crossword.html";
+            break;
+        case ('wordsearch'):
+            window.location = "wordsearch.html";
+            break;
+        case ('sudoku'):
+            window.location = "sudoku.html";
+            break;
+        case ('cubbies'):
+            window.location = "cubbies.html";
+            break;
+        default:
+            alert("Not a valid password.");
+            break;
+    }
 }
