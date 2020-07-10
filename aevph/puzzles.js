d6f08681d1ef594bf0f371dd9fdc79e8da6a2aa7
Anisa <anisaanuar98@gmail.com>
2020-07-09 21:02:12 -0400

    Update puzzles.js

--- aevph/puzzles.js
+++ aevph/puzzles.js
@@ -1,5 +1,5 @@
 function goBack() {
-    window.open('./lobby.html',"_self");
+    window.open('./lobby.html', "_self");
 }
 
 document.getElementById("pswd").addEventListener("keypress", function(e) {
@@ -13,15 +13,16 @@ document.getElementById("pswd").addEventListener("keypress", function(e) {
 document.getElementById("submitButton").addEventListener("click", checkPswd);
 
 function checkPswd() {
+    // if you're reading this, I recommend that you... don't :) pls don't ruin the fun just bc idk how to encrypt things lul
     var password = document.getElementById("pswd").value.toLowerCase();
     switch (password) {
         case ('aevph'):
             window.location = "welcome.html";
             break;
-        case('check my socials'):
-        case('checkmysocials'):
-        	window.location = "jointhehunt.html";
-        	break;
+        case ('check my socials'):
+        case ('checkmysocials'):
+            window.location = "jointhehunt.html";
+            break;
         default:
             alert("Not a valid password.");
             break;
