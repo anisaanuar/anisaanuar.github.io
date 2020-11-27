c2bb0d4037932d2694c62cbef3ea75a7758588a6
Anisa <anisaanuar98@gmail.com>
2020-11-27 02:37:05 -0500

    menu updates

--- portfolio2020/portfolio.js
+++ portfolio2020/portfolio.js
@@ -1,3 +1,23 @@
+var menu = [{
+  label: 'home',
+  link: 'index.html'
+},
+{
+  label: 'about',
+  link: 'about.html'
+},
+{
+  label: 'resume',
+  link: 'resume.html'
+},
+{
+  label: 'contact',
+  link: 'contact.html'
+}
+];
+
+var $header = $('<div class="row no-gutters header"> <div class="header-container"> <a class="col-3 col-lg-2 col-xl-1 logo" href="index.html"></a> <div class="col-9 col-lg-10 col-xl-11"> <div class="menu desktop"> </div> <div class="menu mobile"> <a href="javascript:void(0);" class="icon" onclick="toggleMenu()"> <i class="fa fa-bars"></i> </a> </div> </div> </div> </div> <div id="menuLinks"> <a href="index.html">HOME</a> <a href="about.html">ABOUT</a> <a href="resume.html">RESUME</a> <a href="contact.html">CONTACT</a>');
+
 $(document).ready(function() {
   $('.anim1').delay(0).fadeIn(1200);
   $('.anim2').delay(1200).fadeIn(1200);
@@ -18,7 +38,20 @@ $(document).ready(function() {
     //   newLocation = this.href;
     //   $('body').fadeOut(250, newpage);
     // });
-  });
+
+    $header.appendTo('.container-fluid');
+
+    for (i = 0; i < menu.length; i++) {
+      var menuItem = $('<a href="' + menu[i].link + '">' + menu[i].label + '</a>');
+      menuItem.appendTo('.menu.desktop');
+    }
+
+    var url = window.location.href; 
+    var desktop = $('.menu a').filter(function() {
+      return this.href == url }).addClass('active');
+    var mobile = $('#menuLinks a').filter(function() {
+      return this.href == url }).addClass('active');
+ });
 
 function toggleMenu() {
   var x = document.getElementById("menuLinks");
@@ -27,4 +60,8 @@ function toggleMenu() {
   } else {
     x.style.display = "block";
   }
+}
+
+function goToPage(page) {
+    window.location = page;
 }
